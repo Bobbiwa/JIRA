7eb81a11017a2c650b625be1a74b9c997fcea636
dubin.0116 <dubin.0116@bytedance.com>
2024-10-15 17:49:11 +0800

    feature: 初始化项目

---
+++ craco.config.js
@@ -0,0 +1,11 @@
+const path = require('path')
+module.exports = {
+  webpack: {
+    alias: {
+      // 约定：使用 @ 表示 src 文件所在路径
+      '@': path.resolve(__dirname, 'src'),
+      // 约定：使用 @scss 表示 样式 文件所在路径
+      '@scss': path.resolve(__dirname, 'src', 'assets', 'styles')
+    },
+  },
+}
